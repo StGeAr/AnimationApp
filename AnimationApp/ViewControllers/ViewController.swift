dd53dd99e003be30ca15a8339d0cb78a222322ab
German Stavitskiy <gera_stavickii@mail,.ru>
2022-04-05 17:53:35 +0300

    setup class and corner radius for animationView and animationButton, import "Spring"

--- AnimationApp/ViewControllers/ViewController.swift
+++ AnimationApp/ViewControllers/ViewController.swift
@@ -5,13 +5,25 @@
 //  Created by Герман Ставицкий on 05.04.2022.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
 
+    @IBOutlet var animationDescription: UILabel!
+    @IBOutlet var animationView: SpringView! {
+        didSet {
+            animationView.layer.cornerRadius = 10
+        }
+    }
+    @IBOutlet var animationButton: SpringButton! {
+        didSet {
+            animationButton.layer.cornerRadius = 10
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        animationDescription.text = "Hello"
     }
 
 
